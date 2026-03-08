261b3fd25b9f79d502c7cfee2c2f69cf4b928f2a
bhavyatha-m-19 <bhavs2211@gmail.com>
2026-03-08 20:20:55 +0530

    arrays added

---
+++ Arrays/ArrayCreation.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class ArrayCreation {
+
+    public static void main(String[] args){
+
+        int nums[] = new int[4];
+
+        nums[0] = 2;
+        nums[1] = 4;
+        nums[2] = 5;
+        nums[3] = 10;
+
+        for(int i = 0; i<4; i++){
+            System.out.println(nums[i]);
+        }
+    }
+    
+}
