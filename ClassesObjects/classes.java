da25e5ba4bc087d6bee9a1ca8a6f1715855022d2
bhavyatha-m-19 <bhavs2211@gmail.com>
2026-03-08 16:28:48 +0530

    ClassesObjects updated

---
+++ ClassesObjects/classes.java
@@ -0,0 +1,13 @@
+package ClassesObjects;
+
+public class classes {
+    int id;
+    String name;
+    int age;
+
+    void show(){
+        System.out.println("ID: " + id);
+        System.out.println("NAME: " + name);
+        System.out.println("AGE: " + age);
+    }
+}
---
+++ ClassesObjects/main.java
@@ -0,0 +1,15 @@
+package ClassesObjects;
+
+public class main {
+    public static void main(String[] args){
+        classes c1 =  new classes();
+
+        c1.id = 1;
+        c1.name = "Bhavyatha";
+        c1.age = 23;
+
+        c1.show();
+
+    }
+    
+}
