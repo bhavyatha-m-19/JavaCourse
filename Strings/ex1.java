0c2ca4782ee620fa41b63a5dc9cc15a8328de065
bhavyatha-m-19 <bhavs2211@gmail.com>
2026-03-15 22:58:39 +0530

    Strings

---
+++ Strings/ex1.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class ex1 {
+
+    public static void main(String[] args){
+
+        String name = new String("Bhavyatha");
+
+        System.out.println(name);
+        System.out.println("Hello" + name);
+        System.out.println(name.concat("Manjunath"));
+        System.out.println(name.charAt(2));
+
+    }
+    
+}
